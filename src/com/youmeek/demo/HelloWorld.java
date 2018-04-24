c880dc426bf6a7e4bcbae4d671de2a207cd4430e
heliotrue <helio_true@126.com>
2018-04-24 11:27:48 +0800

    Rejected commit: Default

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -1,13 +1,12 @@
 package com.youmeek.demo;
 
-import java.awt.*;
-
 /**
  * Created by wyhelin on 2018/4/23.
  */
 public class HelloWorld {
     public static void main(String[]  args){
         System.out.print("Hello world !");
+        System.out.print("Ha ha ha  !");
     }
 
 }
