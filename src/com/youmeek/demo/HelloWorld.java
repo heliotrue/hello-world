7ae06813b6e1b7902990ac70e0037843bcb0b8d4
heliotrue <helio_true@126.com>
2018-04-26 11:33:59 +0800

    Rejected commit: Default add 1

--- src/com/youmeek/demo/HelloWorld.java
+++ src/com/youmeek/demo/HelloWorld.java
@@ -5,8 +5,13 @@ package com.youmeek.demo;
  */
 public class HelloWorld {
     public static void main(String[]  args){
-        System.out.print("Hello world !");
-        System.out.print("Ha ha ha  !");
+        System.out.println("Hello world !");
+        int temp1=100;
+        int temp2=200;
+        System.out.println("-----------helio.com-----------temp2值=" + temp1 + "," + "当前类=HelloWorld.main()");
+
+        System.out.println("-----------helio.com-----------temp2值=" + temp2 + "," + "当前类=HelloWorld.main()");
+
     }
 
 }
--- src/com/youmeek/demo/TestGit.java
+++
@@ -1,7 +0,0 @@
-package com.youmeek.demo;
-
-/**
- * Created by wyhelin on 2018/4/24.
- */
-public class TestGit {
-}
